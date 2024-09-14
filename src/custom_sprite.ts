975784c461c93fd504f22e1bceb981969e5c3601
clothtils <clembentine@gmail.com>
2024-09-13 20:02:48 -0700

    Ensure paths are normalized

--- src/custom_sprite.ts
+++ src/custom_sprite.ts
@@ -3,7 +3,10 @@ import path from 'path'
 
 export const get_custom_sprite_path = (asset_path: string): string => {
     const char_flavor = asset_path.split('/original/')[1].split('_c!')[0].split('/')
-    const sprite = asset_path.split('c!')[1]
+
+    const char = path.join('/', char_flavor[1])
+    const flavor = path.join('/', char_flavor[0])
+    const sprite = path.join('/', asset_path.split('c!')[1])
     
-    return path.join('custom_assets', char_flavor[1], char_flavor[0], sprite)
+    return path.join('/', '/custom_assets', char, flavor, sprite)
 }
